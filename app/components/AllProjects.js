71233afa97c230e391c7ba4544c5d18099fbbe84
Jian <jiankuang7@gmail.com>
2021-10-19 15:59:51 -0400

    Updating AllProjects

--- app/components/AllProjects.js
+++ app/components/AllProjects.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchProjects } from "../redux/projects";
 
 // Notice that we're exporting the AllProjects component twice. The named export
 // (below) is not connected to Redux, while the default export (at the very
 // bottom) is connected to Redux. Our tests should cover _both_ cases.
 export class AllProjects extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    this.props.fetchProjects();
+  }
   render() {
-    return <div />;
+    console.log(this.props.projects);
+    return <div></div>;
   }
 }
 
-const mapState = () => {
-  return {};
+const mapState = (state) => {
+  return {
+    projects: state.projects,
+  };
 };
 
-const mapDispatch = () => {
-  return {};
+const mapDispatch = (dispatch) => {
+  return {
+    fetchProjects: () => dispatch(fetchProjects()),
+  };
 };
 
 export default connect(mapState, mapDispatch)(AllProjects);

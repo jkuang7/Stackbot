c638c2852d06ed61d58755e4eb280ad7233ec734
Jian <jiankuang7@gmail.com>
2021-10-19 12:00:50 -0400

    Robots model

---
+++ server/db/robot.js
@@ -0,0 +1,33 @@
+const db = require("./database");
+const Sequelize = require("sequelize");
+
+const Robot = db.define("robot", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  fuelType: {
+    type: Sequelize.STRING,
+    defaultValue: "electric",
+    validate: {
+      isIn: [["gas", "diesel", "electric"]],
+    },
+  },
+  fuelLevel: {
+    type: Sequelize.DECIMAL(10, 2),
+    defaultValue: 100,
+    validate: {
+      isDecimal: true,
+      min: 0,
+      max: 100,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+  },
+});
+
+module.exports = Robot;

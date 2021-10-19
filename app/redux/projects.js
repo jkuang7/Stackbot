f65bd34577def9728dbd2dfd3582c9fcf5770c2c
Jian <jiankuang7@gmail.com>
2021-10-19 12:56:45 -0400

    Adding action types, creators, and thunks for robots and projects

--- app/redux/projects.js
+++ app/redux/projects.js
@@ -1,9 +1,35 @@
-export const setProjects = () => {};
+import Axios from "axios";
 
-export const fetchProjects = () => {};
+//Action Types
+const SET_PROJECTS = "SET_PROJECTS";
+
+//Action Creators
+export const setProjects = (projects) => {
+  return {
+    type: SET_PROJECTS,
+    projects,
+  };
+};
+
+//Thunks
+export const fetchProjects = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await Axios.get("/api/projects");
+      dispatch(setProjects(data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
 
 // Take a look at app/redux/index.js to see where this reducer is
 // added to the Redux store with combineReducers
-export default function projectsReducer() {
-  return null;
+export default function projectsReducer(state = {}, action) {
+  switch (action.type) {
+    case SET_PROJECTS:
+      return { ...state, projects: action.projects };
+    default:
+      return state;
+  }
 }
--- app/redux/robots.js
+++ app/redux/robots.js
@@ -1,14 +1,35 @@
+import Axios from "axios";
+
 //Action Types
 const SET_ROBOTS = "SET_ROBOTS";
 
-//Action Creator
-export const setRobots = () => {};
+//Action Creators
+export const setRobots = (robots) => {
+  return {
+    type: SET_ROBOTS,
+    robots,
+  };
+};
 
 //Thunks
-export const fetchRobots = () => {};
+export const fetchRobots = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await Axios.get("/api/robots");
+      dispatch(setRobots(data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
 
 // Take a look at app/redux/index.js to see where this reducer is
 // added to the Redux store with combineReducers
-export default function robotsReducer() {
-  return null;
+export default function robotsReducer(state = {}, action) {
+  switch (action.type) {
+    case SET_ROBOTS:
+      return { ...state, robots: action.robots };
+    default:
+      return state;
+  }
 }

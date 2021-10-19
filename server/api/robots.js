f81576594ca1885020b1e6832108c334911e803c
Jian <jiankuang7@gmail.com>
2021-10-19 19:09:56 -0400

    single robot route w/ robot's projects

--- server/api/robots.js
+++ server/api/robots.js
@@ -4,9 +4,23 @@ const { Robot, Project } = require("../db/index");
 router.get("/", async (req, res, next) => {
   try {
     const robots = await Robot.findAll({
-      include: {model: Project}
+      include: { model: Project },
     });
     res.json(robots);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const robot = await Robot.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: Project
+    });
+    res.json(robot);
   } catch (err) {
     next(err);
   }

54748d6cb0eff5f260c8531b591e8fec8a0260f4
Jian <jiankuang7@gmail.com>
2021-10-19 19:11:21 -0400

    single project route w/ robots

--- server/api/projects.js
+++ server/api/projects.js
@@ -1,10 +1,24 @@
 const router = require("express").Router();
-const { Project } = require("../db/index");
+const { Project, Robot } = require("../db/index");
 
 router.get("/", async (req, res, next) => {
   try {
     const projects = await Project.findAll();
     res.json(projects);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const robot = await Project.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: Robot
+    });
+    res.json(robot);
   } catch (err) {
     next(err);
   }

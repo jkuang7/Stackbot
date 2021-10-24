b22b23439700a70eb2e0cf8a41d45e20b24ba493
Jian <jiankuang7@gmail.com>
2021-10-23 23:18:46 -0400

    Refactoring code

--- app/redux/projects.js
+++ app/redux/projects.js
@@ -2,20 +2,12 @@ import Axios from "axios";
 
 //Action Types
 const SET_PROJECTS = "SET_PROJECTS";
-const SET_UNRELATED_PROJECTS = "SET_UNRELATED_PROJECTS";
 
 //Action Creators
 export const setProjects = (projects) => {
   return {
     type: SET_PROJECTS,
     projects,
-  };
-};
-
-export const setUnrelatedProjects = (unrelatedProjects) => {
-  return {
-    type: SET_UNRELATED_PROJECTS,
-    unrelatedProjects,
   };
 };
 
@@ -42,25 +34,12 @@ export const fetchProjectsByRobotId = (id) => {
   };
 };
 
-export const fetchProjectsUnrelatedToRobotId = (id) => {
-  return async (dispatch) => {
-    try {
-      const { data } = await Axios.get(`/api/projects/notrobot/${id}`);
-      dispatch(setUnrelatedProjects(data));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-};
-
 // Take a look at app/redux/index.js to see where this reducer is
 // added to the Redux store with combineReducers
 export default function projectsReducer(state = [], action) {
   switch (action.type) {
     case SET_PROJECTS:
       return [...action.projects];
-    case SET_UNRELATED_PROJECTS:
-      return [...action.unrelatedProjects]
     default:
       return state;
   }
--- app/redux/singleProject.js
+++ app/redux/singleProject.js
@@ -69,8 +69,6 @@ export const toggleComplete = (id, completed) => {
   };
 };
 
-
-
 // Take a look at app/redux/index.js to see where this reducer is
 // added to the Redux store with combineReducers
 export default function projectReducer(state = {}, action) {
